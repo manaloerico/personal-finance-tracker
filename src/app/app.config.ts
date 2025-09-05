d820363ebee103cc9850ebf4159fd562b1657632
Echo <manaloerico.games@gmail.com>
2025-09-05 16:58:13 +0800

    setting up firebase

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -2,7 +2,18 @@ import { ApplicationConfig } from '@angular/core';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
+import { provideAnimations } from '@angular/platform-browser/animations';
 
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideFirestore, getFirestore } from '@angular/fire/firestore';
+import { environment } from '../environment/environment';
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+  providers: [
+    provideRouter(routes),
+    provideAnimations(),
+    provideFirebaseApp(() => initializeApp(environment.firebase)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+  ],
 };
---
+++ src/environment/environment.ts
@@ -0,0 +1,13 @@
+// src/environments/environment.ts
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: "AIzaSyCaCyk5zQ_5sYkB0vfC8aQW294GWyhjR8Y",
+    authDomain: "financetracker-b5e5d.firebaseapp.com",
+    projectId: "financetracker-b5e5d",
+    storageBucket: "financetracker-b5e5d.firebasestorage.app",
+    messagingSenderId: "515607980314",
+    appId: "1:515607980314:web:9dc0d70fe7987e0d1164a3",
+    measurementId: "G-E697346JPP"
+  },
+};
